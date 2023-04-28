0b15af5c805ca40700002b3aa850f195d268d3af
Cédric Schieli <cschieli@gmail.com>
2023-04-28 17:48:22 +0200

    Use production firebase in development environment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,11 @@
-export const environment = {};
+export const environment = {
+  firebase: {
+    projectId: 'tbsim-83ed3',
+    appId: '1:983991627957:web:5a2cc3638553c000b00efc',
+    storageBucket: 'tbsim-83ed3.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyDvqn8Jg_AMjKx7Ga1mebPY6QkFI3YWIgI',
+    authDomain: 'tbsim-83ed3.firebaseapp.com',
+    messagingSenderId: '983991627957',
+  },
+};
